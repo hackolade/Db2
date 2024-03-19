041449ee3fb4e91b5e5a9bcdc73125f2821da59c
unknown <unknown>
2024-03-19 11:45:09 +0200

    - Added defaultTypes and descriptors configs - Added reservedWords enum

---
+++ forward_engineering/configs/defaultTypes.js
@@ -0,0 +1,11 @@
+module.exports = {
+	number: 'numeric',
+	string: 'char',
+	date: 'datetime',
+	timestamp: 'datetime',
+	binary: 'binary',
+	boolean: 'boolean',
+	document: 'jsonb',
+	array: 'jsonb',
+	default: 'char',
+};
