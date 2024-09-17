7c685d45bb503e45b1fa65c07de9cae4a6f8001e
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2024-09-17 16:10:32 +0300

    HCK-7990: add comma commenting for last activated column statement

--- forward_engineering/utils/joinActivatedAndDeactivatedStatements.js
+++ forward_engineering/utils/joinActivatedAndDeactivatedStatements.js
@@ -13,8 +13,12 @@ const getDelimiter = ({ index, numberOfStatements, lastIndexOfActivatedStatement
 	const isLastStatement = index === numberOfStatements - 1;
 	const isLastActivatedStatement = index === lastIndexOfActivatedStatement;
 
-	if (isLastStatement || isLastActivatedStatement) {
+	if (isLastStatement) {
 		return '';
+	}
+
+	if (isLastActivatedStatement) {
+		return ' --' + delimiter;
 	}
 
 	return delimiter;
