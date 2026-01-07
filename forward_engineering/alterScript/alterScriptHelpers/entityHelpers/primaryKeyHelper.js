1f2913bcc1d2438f876bafc25b79e96d79955731
Alik Rakhmonov <alik.rahmonov@gmail.com>
2026-01-07 13:10:15 +0100

    fix

--- forward_engineering/alterScript/alterScriptHelpers/entityHelpers/primaryKeyHelper.js
+++ forward_engineering/alterScript/alterScriptHelpers/entityHelpers/primaryKeyHelper.js
@@ -438,7 +438,7 @@ const sortModifyPkConstraints = constraintDtos => {
 			return Number(c2.isDropScript) - Number(c1.isDropScript);
 		}
 		// This sorts all statements based on full table name, ASC
-		return c1.fullTableName < c2.fullTableName;
+		return c1.fullTableName.localeCompare(c2.fullTableName);
 	});
 };
 
