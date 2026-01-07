aba233d40de3d3e3cab136c9428116ac80feca4d
Alik Rakhmonov <alik.rahmonov@gmail.com>
2026-01-07 11:45:48 +0100

    fix

--- forward_engineering/alterScript/alterScriptHelpers/entityHelpers/areConstraintOptionsEqual.js
+++ forward_engineering/alterScript/alterScriptHelpers/entityHelpers/areConstraintOptionsEqual.js
@@ -1,10 +1,7 @@
 const _ = require('lodash');
 
-const areConstraintOptionsEqual = (oldConstraintOptions = [], constraintOptions = []) => {
-	return (
-		oldConstraintOptions.length === constraintOptions.length &&
-		_(oldConstraintOptions).differenceWith(constraintOptions, _.isEqual).isEmpty()
-	);
+const areConstraintOptionsEqual = (oldConstraintOptions, constraintOptions) => {
+	return _(oldConstraintOptions).differenceWith(constraintOptions, _.isEqual).isEmpty();
 };
 
 module.exports = {
