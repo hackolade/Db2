bcbcda2a7bbe3528f0d25fbf951775c64d270367
yevhenii-moroziuk <yevhen.moroziuk@hackolade.com>
2026-02-07 22:01:42 +0200

    HCK-14718: Extended the config with cache & order prop

--- forward_engineering/ddlProvider/ddlHelpers/columnDefinition/getColumnDefault.js
+++ forward_engineering/ddlProvider/ddlHelpers/columnDefinition/getColumnDefault.js
@@ -26,13 +26,15 @@ const isGeneratedAsIdentity = ({ identity, type }) => {
  * @param {{ start?: number, increment?: number, minValue?: number, maxValue?: number, cycle?: string }} param0
  * @returns {string}
  */
-const getIdentityOptions = ({ start, increment, minValue, maxValue, cycle }) => {
+const getIdentityOptions = ({ start, increment, minValue, maxValue, cycle, cacheValue, order }) => {
 	const startWith = start ? `START WITH ${start}` : '';
 	const incrementBy = increment ? `INCREMENT BY ${increment}` : '';
 	const minimumValue = minValue ? `MINVALUE ${minValue}` : '';
 	const maximumValue = maxValue ? `MAXVALUE ${maxValue}` : '';
+	const cache = cacheValue ? `CACHE ${cacheValue}` : 'NO CACHE';
+	const orderValue = order ? 'ORDER' : 'NO ORDER';
 
-	return [startWith, incrementBy, cycle, minimumValue, maximumValue].filter(Boolean).join(', ');
+	return [startWith, incrementBy, cycle, minimumValue, maximumValue, cache, orderValue].filter(Boolean).join(' ');
 };
 
 /**
