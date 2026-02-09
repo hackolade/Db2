a714e114d56a619b876ddecec7c62d1142e37515
Yevhenii Moroziuk <yevhen.moroziuk@hackolade.com>
2026-02-09 14:09:03 +0200

    HCK-14718: Include new properties of `AS IDENTITY` in the script (#86)
    <!--do not remove this marker, its needed to replace info when ticket
    title is updated -->
    <!--jira-description-action-hidden-marker-start-->
    
    <table>
    <td>
    <a href="https://hackolade.atlassian.net/browse/HCK-14718"
    title="HCK-14718" target="_blank"><img alt="Sub-task"
    src="https://hackolade.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10316?size=medium"
    />HCK-14718</a> FE: include new properties of `AS IDENTITY` in the
    script
      </td></table>
      <br />
     
    
    <!--jira-description-action-hidden-marker-end-->
    
    ---------
    
    Co-authored-by: chulanovskyi <oleg.chulanovskyi@binary-studio.com>

--- forward_engineering/ddlProvider/ddlHelpers/columnDefinition/getColumnDefault.js
+++ forward_engineering/ddlProvider/ddlHelpers/columnDefinition/getColumnDefault.js
@@ -26,13 +26,14 @@ const isGeneratedAsIdentity = ({ identity, type }) => {
  * @param {{ start?: number, increment?: number, minValue?: number, maxValue?: number, cycle?: string }} param0
  * @returns {string}
  */
-const getIdentityOptions = ({ start, increment, minValue, maxValue, cycle }) => {
+const getIdentityOptions = ({ start, increment, minValue, maxValue, cycle, cache, cacheValue, order }) => {
 	const startWith = start ? `START WITH ${start}` : '';
 	const incrementBy = increment ? `INCREMENT BY ${increment}` : '';
 	const minimumValue = minValue ? `MINVALUE ${minValue}` : '';
 	const maximumValue = maxValue ? `MAXVALUE ${maxValue}` : '';
+	const cacheOption = cacheValue ? `CACHE ${cacheValue}` : cache;
 
-	return [startWith, incrementBy, cycle, minimumValue, maximumValue].filter(Boolean).join(', ');
+	return [startWith, incrementBy, cycle, minimumValue, maximumValue, cacheOption, order].filter(Boolean).join(', ');
 };
 
 /**
