44b0dd722264939015bbb30d88f14d0199948e00
chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2026-02-10 00:35:47 +0200

    fix: removed extra space after view name

--- shared/helpers/queryHelper.js
+++ shared/helpers/queryHelper.js
@@ -83,7 +83,7 @@ const getGenerateTableDdlQuery = ({ schemaName, tableName, objectType }) => {
 const getSelectTableDdlQuery = ({ opToken, schemaName, objectName, objectType }) => {
 	const predicate =
 		objectType === OBJECT_TYPE.view
-			? `SQL_STMT LIKE 'CREATE%VIEW ${objectName} %'
+			? `SQL_STMT LIKE 'CREATE%VIEW ${objectName}%'
 				OR SQL_STMT LIKE 'COMMENT ON TABLE ${objectName}%'`
 			: `SQL_STMT LIKE '%"${schemaName}"."${objectName}"%'`;
 
